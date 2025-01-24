0a1be311e835aa8c26860f64ac7d33844348bc25
Nguyen Minh Tho <definedentity@proton.me>
2025-01-25 01:01:07 +0700

    Cursor

--- hosts/definedos/stylix/default.nix
+++ hosts/definedos/stylix/default.nix
@@ -12,8 +12,27 @@
     # Wallpaper
     image = ./background.png;
 
+    # Cursor
+    cursor = {
+      name = "Rose Pine Cursor";
+      package = pkgs.rose-pine-cursor;
+    };
+
     # Fonts
     fonts = {
+
+      sizes = {
+        applications = 10;
+        desktop = 10;
+        popups = 10;
+        terminal = 12;
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+
       monospace = {
         package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
         name = "JetBrainsMono Nerd Font Mono";
