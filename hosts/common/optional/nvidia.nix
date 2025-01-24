2168d2671e4567109378a662362ffb7b1724667e
Nguyen Minh Tho <definedentity@proton.me>
2025-01-24 16:13:24 +0700

    Switch to offload mode

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -3,7 +3,10 @@
   hardware.graphics.enable = true;
   hardware.nvidia.open = true;
   hardware.nvidia.prime = {
-    sync.enable = true;
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
 
     nvidiaBusId = "PCI:1:0:0";
     amdgpuBusId = "PCI:6:0:0";
