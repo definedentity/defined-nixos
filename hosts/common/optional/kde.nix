4ae9d45b35984a13becfc20f2635f31e70dcaa92
Nguyen Minh Tho <definedentity@proton.me>
2025-01-27 20:00:22 +0700

    Remove unsued packages from KDE

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -1,5 +1,14 @@
+{ pkgs, ... }:
 {
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   services.desktopManager.plasma6.enable = true;
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    elisa
+    kate
+    okular
+    spectacle
+  ];
 }
