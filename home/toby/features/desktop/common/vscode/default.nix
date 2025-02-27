baf9d4d2e32d7308a168c4f19f6c11f8569e00b3
Nguyen Minh Tho <definedentity@proton.me>
2025-02-27 23:17:21 +0700

    Optimize VSCode

--- home/toby/features/desktop/common/vscode/default.nix
+++ home/toby/features/desktop/common/vscode/default.nix
@@ -4,39 +4,9 @@
   system,
   inputs,
   ...
-}:
-let
-  extensions = inputs.nix-vscode-extensions.extensions.${system};
-
-  extensionsList = with extensions.vscode-marketplace; [
-    streetsidesoftware.code-spell-checker
-    christian-kohler.path-intellisense
-    ritwickdey.liveserver
-    yoavbls.pretty-ts-errors
-    esbenp.prettier-vscode
-    mkhl.direnv
-    formulahendry.auto-rename-tag
-    formulahendry.auto-close-tag
-    formulahendry.auto-complete-tag
-    christian-kohler.npm-intellisense
-    jnoortheen.nix-ide
-    pkief.material-icon-theme
-    eamodio.gitlens
-    ziglang.vscode-zig
-    dsznajder.es7-react-js-snippets
-    aaron-bond.better-comments
-    alefragnani.project-manager
-    tamasfe.even-better-toml
-    bradlc.vscode-tailwindcss
-    nefrob.vscode-just-syntax
-    editorconfig.editorconfig
-    foxundermoon.shell-format
-    dbaeumer.vscode-eslint
-    supermaven.supermaven
-    rust-lang.rust-analyzer
-  ];
-in
-{
+}: let
+  extensionsList = import ./extensions.nix {marketplace = inputs.nix-vscode-extensions.extensions.${system}.vscode-marketplace;};
+in {
   imports = [
     ./keybindings.nix
   ];
@@ -46,9 +16,9 @@ in
     package = unstable-pkgs.vscodium;
 
     userSettings = lib.mkForce (builtins.fromJSON (builtins.readFile ./settings.json));
+    extensions = extensionsList;
 
     enableExtensionUpdateCheck = false;
     enableUpdateCheck = false;
-    extensions = extensionsList;
   };
 }
---
+++ home/toby/features/desktop/common/vscode/extensions.nix
@@ -0,0 +1,28 @@
+{marketplace}:
+with marketplace; [
+  streetsidesoftware.code-spell-checker
+  christian-kohler.path-intellisense
+  ritwickdey.liveserver
+  yoavbls.pretty-ts-errors
+  esbenp.prettier-vscode
+  mkhl.direnv
+  formulahendry.auto-rename-tag
+  formulahendry.auto-close-tag
+  formulahendry.auto-complete-tag
+  christian-kohler.npm-intellisense
+  jnoortheen.nix-ide
+  pkief.material-icon-theme
+  eamodio.gitlens
+  ziglang.vscode-zig
+  dsznajder.es7-react-js-snippets
+  aaron-bond.better-comments
+  alefragnani.project-manager
+  tamasfe.even-better-toml
+  bradlc.vscode-tailwindcss
+  nefrob.vscode-just-syntax
+  editorconfig.editorconfig
+  foxundermoon.shell-format
+  dbaeumer.vscode-eslint
+  supermaven.supermaven
+  rust-lang.rust-analyzer
+]
