492e368a7e9dacade8d08f9ecd91a0535b09b3c4
Nguyen Minh Tho <definedentity@proton.me>
2025-03-06 12:40:24 +0700

    Add Davinci Resolve

--- home/toby/features/desktop/games/prism-launcher.nix
+++ home/toby/features/desktop/games/prism-launcher.nix
@@ -1,12 +1,5 @@
-{
-  pkgs,
-  unstable-pkgs,
-  ...
-}: {
-  home.packages = with pkgs; [
-    (prismlauncher.override
-      {
-        jdks = with unstable-pkgs.graalvmPackages; [graalvm-oracle_17 graalvm-oracle];
-      })
+{unstable-pkgs, ...}: {
+  home.packages = with unstable-pkgs; [
+    prismlauncher
   ];
 }
