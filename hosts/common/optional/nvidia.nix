61d697e39655bc4c350ebbbb21e97a7fea397e40
Nguyen Minh Tho <definedentity@proton.me>
2025-03-14 19:58:40 +0700

    Update NVIDIA

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -4,15 +4,5 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     modesetting.enable = true;
-
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-
-      nvidiaBusId = "PCI:1:0:0";
-      amdgpuBusId = "PCI:6:0:0";
-    };
   };
 }
--- hosts/deuterium/default.nix
+++ hosts/deuterium/default.nix
@@ -20,6 +20,17 @@
     ../common/optional/tlp.nix
   ];
 
+  # NVIDIA
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+
+    nvidiaBusId = "PCI:1:0:0";
+    amdgpuBusId = "PCI:6:0:0";
+  };
+
   networking.hostName = "deuterium";
 
   nix.settings.experimental-features = [
