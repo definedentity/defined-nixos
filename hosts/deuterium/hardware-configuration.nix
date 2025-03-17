8850a889e5126fb987e32a28e2a023cf38713002
Nguyen Minh Tho <definedentity@proton.me>
2025-03-17 11:57:04 +0700

    Update hardware config

--- hosts/deuterium/hardware-configuration.nix
+++ hosts/deuterium/hardware-configuration.nix
@@ -32,25 +32,15 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/50332c87-b33f-40b7-b4f0-17553b221651";
+      device = "/dev/disk/by-uuid/0b8f382c-06ca-42ac-be34-0a8bca3ca4dc";
       fsType = "btrfs";
       options = ["subvol=@"];
     };
 
     "/boot" = {
-      device = "/dev/disk/by-uuid/66A7-EB05";
+      device = "/dev/disk/by-uuid/6362-6263";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
-    };
-
-    "/mnt/ssd" = {
-      device = "/dev/disk/by-uuid/30abf803-a248-45a6-8997-9980597dd660";
-      fsType = "btrfs";
-    };
-
-    "/mnt/hdd" = {
-      device = "/dev/disk/by-uuid/38bac5d3-f0d5-41a8-a73a-dfa923531b17";
-      fsType = "btrfs";
     };
   };
 
