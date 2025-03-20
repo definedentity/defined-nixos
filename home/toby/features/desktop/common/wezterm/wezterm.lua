5e857d4ac122b2fe29c86d99ee16b20e6a6c959d
Nguyen Minh Tho <definedentity@proton.me>
2025-03-20 17:27:35 +0700

    Update WezTerm

--- home/toby/features/desktop/common/wezterm/wezterm.lua
+++ home/toby/features/desktop/common/wezterm/wezterm.lua
@@ -3,33 +3,33 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local theme = {
-	name = "macchiato",
-	rosewater = "#f4dbd6",
-	flamingo = "#f0c6c6",
-	pink = "#f5bde6",
-	mauve = "#c6a0f6",
-	red = "#ed8796",
-	maroon = "#ee99a0",
-	peach = "#f5a97f",
-	yellow = "#eed49f",
-	green = "#a6da95",
-	teal = "#8bd5ca",
-	sky = "#91d7e3",
-	sapphire = "#7dc4e4",
-	blue = "#8aadf4",
-	lavender = "#b7bdf8",
-	text = "#cad3f5",
-	subtext1 = "#b8c0e0",
-	subtext0 = "#a5adcb",
-	overlay2 = "#939ab7",
-	overlay1 = "#8087a2",
-	overlay0 = "#6e738d",
-	surface2 = "#5b6078",
-	surface1 = "#494d64",
-	surface0 = "#363a4f",
-	base = "#24273a",
-	mantle = "#1e2030",
-	crust = "#181926",
+    name = "macchiato",
+    rosewater = "#f4dbd6",
+    flamingo = "#f0c6c6",
+    pink = "#f5bde6",
+    mauve = "#c6a0f6",
+    red = "#ed8796",
+    maroon = "#ee99a0",
+    peach = "#f5a97f",
+    yellow = "#eed49f",
+    green = "#a6da95",
+    teal = "#8bd5ca",
+    sky = "#91d7e3",
+    sapphire = "#7dc4e4",
+    blue = "#8aadf4",
+    lavender = "#b7bdf8",
+    text = "#cad3f5",
+    subtext1 = "#b8c0e0",
+    subtext0 = "#a5adcb",
+    overlay2 = "#939ab7",
+    overlay1 = "#8087a2",
+    overlay0 = "#6e738d",
+    surface2 = "#5b6078",
+    surface1 = "#494d64",
+    surface0 = "#363a4f",
+    base = "#24273a",
+    mantle = "#1e2030",
+    crust = "#181926",
 }
 
 local config = {}
@@ -38,25 +38,25 @@ local background = theme.crust
 
 config.color_scheme = "Catppuccin Macchiato"
 config.colors = {
-	background = background,
-	tab_bar = {
-		background = background,
-		active_tab = {
-			bg_color = theme.blue,
-			fg_color = background,
-			intensity = "Bold",
-		},
-		inactive_tab = {
-			fg_color = theme.overlay0,
-			bg_color = background,
-			italic = true,
-		},
-	},
+    background = background,
+    tab_bar = {
+        background = background,
+        active_tab = {
+            bg_color = theme.blue,
+            fg_color = background,
+            intensity = "Bold",
+        },
+        inactive_tab = {
+            fg_color = theme.overlay0,
+            bg_color = background,
+            italic = true,
+        },
+    },
 }
 
 config.check_for_updates = false
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.8
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
@@ -139,30 +139,30 @@ config.keys = {
         key = "]",
         action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
     }, {
-        mods = "ALT",
-        key = "c",
-        action = act.CloseCurrentPane { confirm = true },
-    },
+    mods = "ALT",
+    key = "c",
+    action = act.CloseCurrentPane { confirm = true },
+},
     {
         key = 'h',
         mods = 'ALT',
         action = act.ActivatePaneDirection 'Left',
-      },
-      {
+    },
+    {
         key = 'l',
         mods = 'ALT',
         action = act.ActivatePaneDirection 'Right',
-      },
-      {
+    },
+    {
         key = 'k',
         mods = 'ALT',
         action = act.ActivatePaneDirection 'Up',
-      },
-      {
+    },
+    {
         key = "j",
         mods = "ALT",
         action = act.ActivatePaneDirection "Down",
-      },
+    },
 }
 
 return config
